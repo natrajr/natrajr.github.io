b8c21828564b8204fef7d39bdd3e136a81b7d83b
Rohan Natraj <ron4trj@gmail.com>
2014-04-02 00:04:12 -0400

    issue with display_line

--- transit/maps.js
+++ transit/maps.js
@@ -117,6 +117,7 @@ function init_data() {
 	xhr.onreadystatechange= function() {
 		if (xhr.readyState==4 && xhr.status==200) {
 			mbtaData=JSON.parse(xhr.responseText);
+			whichLine();
 		}
 		else if (xhr.readyState==4 && xhr.status==500) {
 			alert("Error Retrieving MBTA Data");
@@ -124,7 +125,6 @@ function init_data() {
 	}
 	xhr.open("GET", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
 	xhr.send(null);
-	whichLine();
 }
 
 function getDistance(lat1, lng1, lat2, lng2) {
