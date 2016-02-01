15eae4abe3a440b9e79507538ca8137107799b6c
ron4trj <ron4trj@gmail.com>
2016-02-01 15:58:34 -0800

    syntax

--- boxlayout.js
+++ boxlayout.js
@@ -128,17 +128,3 @@ var Boxlayout = (function() {
 	return { init : init };
 
 })();
-
-
-
-
-/**
- * boxlayout.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
