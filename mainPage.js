bfed6bd1e0480b04e257cd0ceaf71472726b113e
Rohan <ron4trj@gmail.com>
2016-03-14 17:04:35 -0700

    layout

---
+++ mainPage.js
@@ -0,0 +1,25 @@
+//need to create a pager function. If the "Next" button is pressed, go to the
+//subsequent page. If the "prev" button is pressed, go to the previous page
+/*
+(()=> {
+    $(document).find(".inactive").hide();
+  }
+)();
+
+$('#previous-button').on('click', function() {
+  var $currentPage=$('.active');
+  var $nextPage="#"+$currentPage.attr('previous');
+  $currentPage.fadeOut(300);
+  $($nextPage).fadeIn(200);
+  $currentPage.removeClass('active').addClass('inactive');
+  $($nextPage).removeClass('inactive').addClass('active');
+});
+$('#next-button').on('click', function() {
+  var $currentPage=$('.active');
+  var $nextPage="#"+$currentPage.attr('next');
+  $currentPage.fadeOut(300);
+  $($nextPage).fadeIn(200);
+  $currentPage.removeClass('active').addClass('inactive');
+  $($nextPage).removeClass('inactive').addClass('active');
+});
+*/
